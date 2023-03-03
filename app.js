7d31f195263a53cd7f9aa96b007373fb915b6493
noyon344351 <mdmehedi121888@gmail.com>
2023-03-03 11:38:20 +0600

    d

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const loadData = () => {
+
+const toggleSpinner = isLoading => {
+  const loadSpinner = document.getElementById('loader');
+  if(isLoading)
+  loadSpinner.classList.remove('d-none');
+  else
+  loadSpinner.classList.add('d-none');
+}
+
+const loadData = (dataLimit) => {
+  toggleSpinner(true);
   fetch("https://openapi.programming-hero.com/api/ai/tools")
     .then((res) => res.json())
-    .then((data) => displayData(data.data.tools));
+    .then((data) => displayData(data.data.tools,dataLimit));
 };
-const displayData = (datas) => {
+const displayData = (datas,dataLimit) => {
   const cardContainer = document.getElementById("card-container");
-  for (const data of datas.slice(0,6)) {
+  for (const data of datas.slice(0,dataLimit)) {
     console.log(data.published_in);
     const featuresLength = data.features;
     const div = document.createElement("div");
     div.classList.add("col");
+
     div.innerHTML = `
         <div class="card">
                         <img src="${data.image}" class="card-img-top" style="width:460px; height:260px">
@@ -18,7 +29,8 @@ const displayData = (datas) => {
                          <ol>
                          <li>${data.features[0]}</li>
                          <li>${data.features[1]}</li>
-                         <li>${data.features[2]}</li>
+                    
+                           <li>${data.features[2] ? data.features[2] : ''}</li>
                          </ol>
                         <hr class="my-3">
                         <h5 class="card-title mb-3">${data.name}</h5>
@@ -34,4 +46,12 @@ const displayData = (datas) => {
     cardContainer.appendChild(div);
   }
 };
-loadData();
+loadData(6);
+toggleSpinner(false);
+const seeMore = () =>{
+  // toggleSpinner(true);
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerText = '';
+  loadData();
+  toggleSpinner(false);
+}
